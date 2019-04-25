7053078d1cd35543a30a519253aa9ca2ed3e53f7
minhduc2611 <minhduc261298@gmail.com>
2019-04-25 20:53:31 +0700

    add viewComponent, on tap linq va toan tu lambda

---
+++ aspNhatnghe/other/ViewComponent/datafirst/Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datafirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datafirst.Controllers
+{
+    public class DemoController : Controller
+    {
+        private readonly MyeStoreContext ctx;
+
+        public DemoController(MyeStoreContext db) {
+            ctx = db;
+        }
+
+        // https://localhost:44397/demo/ThongKe
+        public IActionResult ThongKe()
+        {
+            var data = ctx.HangHoa
+                .GroupBy(p => p.MaLoaiNavigation)
+                .Select(p => new {
+                    p.Key.MaLoai,
+                    p.Key.TenLoai,
+                    p.Key.MoTa,
+                    SoMatHang = p.Count(),
+                    TongSoLanXem = p.Sum(s => s.SoLanXem),
+                    GiaTB = p.Average(a => a.DonGia.Value)
+                    });
+            return Json(data);
+        }
+        
+    }
+}
